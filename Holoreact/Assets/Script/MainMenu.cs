87b2d95663139825e78904da188afd6031161528
nalinai <samueledselfernandez@gmail.com>
2020-09-05 20:51:40 +0700

    Create main menu and scene manager script and create gradient editor script
    all the changes could be seen at Test maim menu script scene

---
+++ Holoreact/Assets/Script/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// this script is used to manage MainMenu scene
+/// </summary>
+
+public class MainMenu : MonoBehaviour
+{   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //keycode Return mean enter since keycode enter in unity refer to enter in the numpad
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            SCENE_MANAGER.NextScene();
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SCENE_MANAGER.Quit();
+        }
+    }
+}
---
+++ Holoreact/Assets/Script/SCENE_MANAGER.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This script is used to manage scene.
+/// eg : nextscene() is used to move to next scene.
+/// </summary>
+
+public static class SCENE_MANAGER
+{
+    private static int index = 0;
+    
+    public static void NextScene()
+    {
+        index++;
+        MoveScene();
+    }
+
+    public static void PrevScene()
+    {
+        index--;
+        MoveScene();
+    }
+
+    public static void BackToMenu()
+    {
+        index = 0;
+        MoveScene();
+    }
+
+    public static void Quit()
+    {
+        Application.Quit();
+    }
+
+    private static void MoveScene()
+    {
+        SceneManager.LoadScene(index);
+    }
+
+}
