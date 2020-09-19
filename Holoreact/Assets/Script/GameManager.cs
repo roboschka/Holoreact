860be85e85ecc97b7af220522a0da9c63cd5c0ab
nalinai <samueledselfernandez@gmail.com>
2020-09-19 19:05:51 +0700

    Gamemanager empty script and JsonHelper
    Create JsonHelper script
    create Gamanager script for preparation

---
+++ Holoreact/Assets/Script/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] tools;
+    [SerializeField]
+    private GameObject[] material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tools = GameObject.FindGameObjectsWithTag("Tools");
+        material = GameObject.FindGameObjectsWithTag("Material");
+    }
+
+    private GameObject FindResult(GameObject tool, GameObject material)
+    {
+
+        //the logic will goes to here
+        //find the result from array using linQ
+        return null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
