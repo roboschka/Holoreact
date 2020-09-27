5c750f20a2fd29eb91f6807c3585fe8512575d68
nalinai <samueledselfernandez@gmail.com>
2020-09-27 10:58:24 +0700

    Create GameManager get data from api
    Create gamemanger get data from api and create search result function

---
+++ Holoreact/Assets/Script/DataFromAPI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public class DataFromAPI
+{
+    public string ItemA { get; set; }
+    public string ItemB { get; set; }
+    public string Result { get; set; }
+}
