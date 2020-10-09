a1417550acdbb8beb5e1d6b7eea20013793be1c1
nalinai <samueledselfernandez@gmail.com>
2020-10-09 10:47:34 +0700

    Create setting script and the scene for test

---
+++ Holoreact/Assets/Script/Setting.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setting : MonoBehaviour
+{
+
+    private bool toggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            ToggleSound();
+        }
+    }
+
+    private void ToggleSound()
+    {
+        toggle = !toggle;
+
+        if (toggle)
+            AudioListener.volume = 1f;
+        else
+            AudioListener.volume = 0f;
+    }
+
+}
